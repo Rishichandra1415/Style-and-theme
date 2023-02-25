d614b988b014e72a5bc353810192ebe50d677502
rishi <rishichandra1415@gmail.com>
2023-02-25 11:59:47 +0530

    in this wecan us estyle and theme

---
+++ app/src/main/java/com/example/themeandstyle/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.themeandstyle;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
+
+
+//ALL THR THING THAT WE CAN DOO  HERE.......
+
+
+// in this we can chnge the theme and style  in universal method in this we can go to the res file and then select the STYLE attribute and then  use ITEM attribute and then chang eaccourdin  to the
